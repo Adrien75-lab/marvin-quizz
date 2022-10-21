7fecc7ec0807480378d9414609e599b2a15f8106
Adrien Christophe <a.christophe@webintelligence.fr>
2022-10-21 16:59:49 +0200

    first commit

---
+++ src/components/Firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyCRLcA32n6KE-7B8kyVfUAImXmYzlopyrw",
+    authDomain: "marvel-quizz-ff5aa.firebaseapp.com",
+    projectId: "marvel-quizz-ff5aa",
+    storageBucket: "marvel-quizz-ff5aa.appspot.com",
+    messagingSenderId: "840741791916",
+    appId: "1:840741791916:web:94cfccd7e5aaa5859c5709"
+}
+
+const app = initializeApp(config);
---
+++ src/components/Login/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className="signUpLoginBox">Login
+            <div className="slContainer">
+                Login
+            </div>
+        </div>
+    )
+}
+
+export default Login
